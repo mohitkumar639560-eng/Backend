d83ca28fb0415fa8be3641fbf8a4f80318c1363e
mohit kumar <Escape>
2026-05-28 11:47:47 +0530

    removed files and updated project

--- server.js
+++
@@ -1,9 +0,0 @@
-import app from "./src/app.js"
-import connectDB from "./src/config/db.js";
-
-
-await connectDB();
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
